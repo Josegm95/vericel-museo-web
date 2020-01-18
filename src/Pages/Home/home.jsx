eec1e589c5e8e206e84f90019775f1628138e986
Jose Gomez <jose.fgm1995@gmail.com>
2020-01-18 11:57:16 -0500

    prismic implemented

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { usePrismicRequest } from '../../Requests/prismic';
 import './home.scss';
 
 const Home = () => {
+  const [data, loading] = usePrismicRequest('especimen');
+
+  useEffect(() => {
+    if (!loading && data) {
+      console.log(data);
+    }
+  });
+
   return <section className="home-container">Home</section>;
 };
 
---
+++ src/Requests/prismic.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import Prismic from 'prismic-javascript';
+
+export const usePrismicRequest = colection => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const apiEndpoint = 'https://museovericel.cdn.prismic.io/api/v2';
+  const accessToken =
+    'MC5YaU1SZUJBQUFDVUFmSlNw.A--_vQbvv71laWjvv70VDABC77-977-9Pxfvv73vv71t77-9XGTvv73vv73vv73vv73vv71LMXrvv70i';
+
+  useEffect(() => {
+    setLoading(true);
+    const Client = Prismic.client(apiEndpoint, { accessToken });
+
+    const fetchData = async () => {
+      const response = await Client.query(
+        Prismic.Predicates.at('document.type', colection)
+      );
+      if (response) {
+        setData(response);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [colection]);
+
+  return [data, loading];
+};
