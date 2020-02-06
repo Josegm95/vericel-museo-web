90b132c0c8be59ddc4f4ff5ed7e9b09c5ff8ea7c
Jose Gomez <jose.fgm1995@gmail.com>
2020-02-06 15:22:01 -0500

    build folder deleted from gitignore

---
+++ build/precache-manifest.d16f683b17c190e6dcc0e81c9e970720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91330f36835d0cfd71ce7d92ac12d32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f349702d51390900e06",
+    "url": "/static/css/main.3f9d1b64.chunk.css"
+  },
+  {
+    "revision": "4e8d01d151bd35c36217",
+    "url": "/static/js/2.341d84d5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.341d84d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f349702d51390900e06",
+    "url": "/static/js/main.f30289ef.chunk.js"
+  },
+  {
+    "revision": "af1a6f8c89b9659b6e20",
+    "url": "/static/js/runtime-main.3966d7d1.js"
+  }
+]);
